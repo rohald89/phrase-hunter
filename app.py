4e3f5451d4501a79ea6aade7c9623943cf5f1c63
Rohald van Merode <rohald_89@live.nl>
2021-10-09 23:47:22 +0200

    created the __init__ methods for phrase and game classes and create_phrases method

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 # Import your Game class
-
+from phrasehunter.game import Game
+from phrasehunter.phrase import Phrase
 # Create your Dunder Main statement.
-
+if __name__ == '__main__':
+    # phrase = Phrase()
+    game = Game()
+    phrase = Phrase('Life is like a box of chocolates')
+    for phrase in game.phrases:
+        print(phrase.phrase)
+        
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,15 @@
-# Create your Game class logic in here.
+from phrasehunter.phrase import Phrase
+
+class Game:
+    def __init__(self):
+        self.missed = 0
+        self.phrases = self.create_phrases()
+        self.active_phrase = None
+        self.guesses = [" "]
+
+    def create_phrases(self):
+        return [Phrase("I love Python"),
+        Phrase("testing phrases "),
+        Phrase("This is another phrase"),
+        Phrase("How many more do I need"),
+        Phrase("This will be the last one")]
