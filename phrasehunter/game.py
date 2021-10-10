343494d57ebce7af3c55902798aa4cac157db483
Rohald van Merode <rohald_89@live.nl>
2021-10-10 11:40:14 +0200

    created the methods for testing guesses and implemented reset functionality

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,10 +1,13 @@
+import random
+import string
+
 from phrasehunter.phrase import Phrase
 
 class Game:
     def __init__(self):
         self.missed = 0
         self.phrases = self.create_phrases()
-        self.active_phrase = None
+        self.active_phrase = self.get_random_phrase()
         self.guesses = [" "]
 
     def create_phrases(self):
@@ -13,3 +16,53 @@ class Game:
         Phrase("This is another phrase"),
         Phrase("How many more do I need"),
         Phrase("This will be the last one")]
+
+    def get_random_phrase(self):
+        return random.choice(self.phrases)
+
+    def welcome(self):
+        print('''
+        ============================
+          Welcome to Phrase Hunter
+        ============================
+        ''')
+
+    def start(self):
+        self.welcome()
+        while self.missed < 5 and not self.active_phrase.check_complete(self.guesses):
+            print(f"You have {5 - self.missed} out of 5 lives remaining!")
+            self.active_phrase.display(self.guesses)
+            user_guess = self.get_guess()
+            self.guesses.append(user_guess)
+            if not self.active_phrase.check_guess(user_guess):
+                self.missed += 1
+        self.game_over()
+        self.replay()
+
+    def get_guess(self):
+        while True:
+            guess = input('\nEnter a letter: ').lower()
+            if len(guess) > 1 or guess.lower() not in string.ascii_lowercase:
+                print("Please enter a valid character a-z")
+                continue
+            else:
+                return guess
+
+    def game_over(self):
+        if self.missed == 5:
+            print("You've lost, better luck next time!")
+        else:
+            print("Congrats! You've won!")
+
+    def replay(self):
+        replay = input("Would you like to play again? Y/N ")
+        if replay.lower() == 'y':
+            self.reset()
+        else:
+            print("Thanks for playing! Hope to see you back soon")
+    
+    def reset(self):
+        self.missed = 0
+        self.active_phrase = self.get_random_phrase()
+        self.guesses = [" "]
+        self.start()
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -2,3 +2,16 @@
 class Phrase:
     def __init__(self, phrase):
         self.phrase = phrase.lower()
+
+    def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
+                print(f"{letter} ", end='')
+            else:
+                print('_', end=' ')
+
+    def check_guess(self, guess):
+        return guess in self.phrase
+
+    def check_complete(self, guesses):
+        return set(guesses).issuperset(set(self.phrase))
